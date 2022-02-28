c54342ce4d72643742c9a280ed7a510e75feaa8c
Sean Petrie <sean.petrie17@gmail.com>
2022-02-27 19:09:22 -0600

    Added new events

---
+++ TabletopTweaks-Core/NewEvents/IBlueprintCacheInitHandler.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.PubSubSystem;
+
+namespace TabletopTweaks.Core.NewEvents {
+    interface IBlueprintCacheInitHandler : IGlobalSubscriber {
+        void BeforeBlueprintCachePatches();
+        void BeforeBlueprintCacheInit();
+        void AfterBlueprintCacheInit();
+        void AfterBlueprintCachePatches();
+
+        [HarmonyPatch(typeof(BlueprintsCache), nameof(BlueprintsCache.Init))]
+        internal static class BlueprintsCache_Init_EventHandler {
+            
+            static void Prefix() {
+                EventBus.RaiseEvent<IBlueprintCacheInitHandler>(h => h.BeforeBlueprintCacheInit());
+            }
+
+            static void Postfix() {
+                EventBus.RaiseEvent<IBlueprintCacheInitHandler>(h => h.AfterBlueprintCacheInit());
+            }
+        }
+        [HarmonyPatch(typeof(StartGameLoader), nameof(StartGameLoader.LoadPackTOC))]
+        internal class StartGameLoader_LoadPackTOC_EventHandler {
+            static void Prefix() {
+                EventBus.RaiseEvent<IBlueprintCacheInitHandler>(h => h.BeforeBlueprintCachePatches());
+            }
+
+            static void Postfix() {
+                EventBus.RaiseEvent<IBlueprintCacheInitHandler>(h => h.AfterBlueprintCachePatches());
+            }
+        }
+    }
+}
---
+++ TabletopTweaks-Core/NewEvents/ILocaleChangedHandler.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Kingmaker.Localization;
+using Kingmaker.PubSubSystem;
+
+namespace TabletopTweaks.Core.NewEvents {
+    interface ILocaleChangedHandler : IGlobalSubscriber {
+        void HandleLocaleChanged();
+
+        [HarmonyPatch(typeof(LocalizationManager), nameof(LocalizationManager.OnLocaleChanged))]
+        internal static class LocalizationManager_OnLocaleChanged_Handler {
+            static void Postfix() {
+                EventBus.RaiseEvent<ILocaleChangedHandler>(h => h.HandleLocaleChanged());
+            }
+        }
+    }
+}
