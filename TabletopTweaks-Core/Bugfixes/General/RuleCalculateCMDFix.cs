0599d99660147a7b4afa60cc2dd9840be9b11ad2
Sean Petrie <sean.petrie17@gmail.com>
2022-03-25 21:21:54 -0500

    Feature support for Armor Mastery

---
+++ TabletopTweaks-Core/Bugfixes/General/RuleCalculateCMDFix.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Kingmaker.RuleSystem.Rules;
+
+namespace TabletopTweaks.Core.Bugfixes.General {
+    class RuleCalculateCMDFix {
+        [HarmonyPatch(typeof(RuleCalculateCMD), nameof(RuleCalculateCMD.Result), MethodType.Getter)]
+        static class RuleCalculateCMD_Result_Fix {
+            static void Postfix(RuleCalculateCMD __instance, ref int __result) {
+                __result = __instance.Base.Result + __instance.TotalBonusValue;
+            }
+        }
+    }
+}
---
+++ TabletopTweaks-Core/NewComponents/ContextConditionSizeDifferneceFromCaster.cs
@@ -0,0 +1,26 @@
+﻿using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.UnitLogic.Mechanics.Conditions;
+
+namespace TabletopTweaks.Core.NewComponents {
+    [TypeId("1012b102496f4bfd98c0b854bfe5f98e")]
+    public class ContextConditionSizeDifferneceFromCaster : ContextCondition {
+        public override bool CheckCondition() {
+            var casterSize = base.Context.MaybeCaster?.State.Size ?? 0;
+            var targetSize = base.Target?.Unit?.State.Size ?? 0;
+
+            if (delta == 0) {
+                return casterSize == targetSize;
+            }
+            if (delta > 0) {
+                return (targetSize - casterSize) <= delta;
+            }
+            return (casterSize - targetSize) <= delta;
+        }
+
+        public override string GetConditionCaption() {
+            return "Check if target has size delta in range of caster";
+        }
+
+        public int delta;
+    }
+}
