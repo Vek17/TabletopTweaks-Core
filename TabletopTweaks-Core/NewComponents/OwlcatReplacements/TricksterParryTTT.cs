c9aa070dc4c2d3977e2d963845ba8946fe67c96d
Sean Petrie <sean.petrie17@gmail.com>
2022-04-08 20:13:19 -0500

    Added support for new trickster customization

---
+++ TabletopTweaks-Core/NewComponents/OwlcatReplacements/TricksterParryTTT.cs
@@ -0,0 +1,36 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Facts;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.Enums;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic;
+
+namespace TabletopTweaks.Core.NewComponents.OwlcatReplacements {
+    [AllowedOn(typeof(BlueprintUnitFact), false)]
+    [TypeId("30b36119dec242d19273366e0baebbb7")]
+    public class TricksterParryTTT : UnitFactComponentDelegate, 
+        ITargetRulebookHandler<RuleAttackRoll>, 
+        ISubscriber, ITargetRulebookSubscriber {
+        public void OnEventAboutToTrigger(RuleAttackRoll evt) {
+        }
+
+        public void OnEventDidTrigger(RuleAttackRoll evt) {
+            if (!evt.IsHit || evt.IsFake) {
+                return;
+            }
+            int dc = evt.D20 + evt.AttackBonus;
+            var ruleSkillCheck = new RuleSkillCheck(base.Owner, StatType.SkillMobility, dc);
+            var modifier = base.Owner.Stats.SkillMobility.AddModifier(-10, base.Runtime, ModifierDescriptor.Penalty);
+            ruleSkillCheck.AddTemporaryModifier(modifier);
+            Rulebook.Trigger<RuleSkillCheck>(ruleSkillCheck);
+            if (ruleSkillCheck.Success) {
+                evt.AutoMiss = true;
+            }
+        }
+
+        public int PenaltyValue = -10;
+    }
+}
