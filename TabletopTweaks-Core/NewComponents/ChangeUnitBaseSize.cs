24d3ab36a8b0ba32d85a73058c011b1a29619f5d
Sean Petrie <sean.petrie17@gmail.com>
2022-04-11 16:06:35 -0500

    Added support for base size adjustments that stack with normal size changes

---
+++ TabletopTweaks-Core/NewComponents/ChangeUnitBaseSize.cs
@@ -0,0 +1,44 @@
+﻿using JetBrains.Annotations;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Facts;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Enums;
+using Kingmaker.UnitLogic;
+using Kingmaker.Utility;
+using TabletopTweaks.Core.NewUnitParts;
+using UnityEngine;
+
+
+namespace TabletopTweaks.Core.NewComponents {
+    [AllowedOn(typeof(BlueprintUnitFact), false)]
+    [TypeId("bc8e9ee439304cf8a208f32f1f116f3a")]
+    public class ChangeUnitBaseSize : UnitFactComponentDelegate {
+        [UsedImplicitly]
+        private bool IsTypeValue => m_Type == UnitPartBaseSizeAdjustment.ChangeType.Value;
+
+        [UsedImplicitly]
+        private bool IsTypeDelta => m_Type == UnitPartBaseSizeAdjustment.ChangeType.Delta;
+
+        public override void OnTurnOn() {
+            if (IsTypeValue) {
+                base.Owner.Ensure<UnitPartBaseSizeAdjustment>().AddEntry(Size, base.Fact);
+            }
+            if (IsTypeDelta) {
+                base.Owner.Ensure<UnitPartBaseSizeAdjustment>().AddEntry(SizeDelta, base.Fact);
+            }
+        }
+
+        public override void OnTurnOff() {
+            base.Owner.Ensure<UnitPartBaseSizeAdjustment>().RemoveEntry(base.Fact);
+        }
+
+        [SerializeField]
+        public UnitPartBaseSizeAdjustment.ChangeType m_Type;
+
+        [ShowIf("IsTypeDelta")]
+        public int SizeDelta;
+
+        [ShowIf("IsTypeValue")]
+        public Size Size;
+    }
+}
---
+++ TabletopTweaks-Core/NewUnitParts/UnitPartBaseSizeAdjustment.cs
@@ -0,0 +1,105 @@
+﻿using HarmonyLib;
+using Kingmaker.EntitySystem;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.Enums;
+using Kingmaker.PubSubSystem;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Parts;
+using Kingmaker.Utility;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TabletopTweaks.Core.NewUnitParts {
+    public class UnitPartBaseSizeAdjustment : OldStyleUnitPart, IUnitSizeHandler {
+        public Size GetAdjustedSize(Size originalSize) {
+            var adjustment = Adjustments.LastItem();
+            if (adjustment == null) { return originalSize; }
+            if (adjustment.Type == ChangeType.Value) {
+                return adjustment.Size;
+            }
+            Size result = originalSize.Shift(adjustment.SizeDelta);
+            return result;
+        }
+        public void AddEntry(int sizeDelta, EntityFact source) {
+            Adjustments.Add(new BaseSizeAdjustmentEntry(sizeDelta, source));
+            skipEvent = true;
+            UpdateSize();
+        }
+        public void AddEntry(Size size, EntityFact source) {
+            Adjustments.Add(new BaseSizeAdjustmentEntry(size, source));
+            skipEvent = true;
+            UpdateSize();
+        }
+        public void RemoveEntry(EntityFact source) {
+            Adjustments.RemoveAll((BaseSizeAdjustmentEntry c) => c.Source == source);
+            skipEvent = true;
+            UpdateSize();
+            TryRemove();
+        }
+        private void TryRemove() {
+            if (!Adjustments.Any()) { this.RemoveSelf(); }
+        }
+        private void UpdateSize() {
+            base.Owner.State.Size = Owner.OriginalSize;
+            var adjustment = this.Adjustments.LastItem();
+            var changeSizePart = Owner.Get<UnitPartSizeModifier>();
+            if (adjustment == null) {
+                if (changeSizePart != null) {
+                    changeSizePart.UpdateSize();
+                }
+                return;
+            }
+            if (adjustment.Type == ChangeType.Value) {
+                base.Owner.State.Size = GetAdjustedSize(Owner.State.Size);
+                if (changeSizePart != null) {
+                    changeSizePart.UpdateSize();
+                }
+                return;
+            }
+            if (changeSizePart != null) {
+                changeSizePart.UpdateSize();
+            }
+            base.Owner.State.Size = GetAdjustedSize(Owner.State.Size);
+            skipEvent = false;
+        }
+
+        public void HandleUnitSizeChanged(UnitEntityData unit) {
+            if (unit == Owner) {
+                if (skipEvent) { return; }
+                skipEvent = true;
+                UpdateSize();
+            }
+        }
+        private bool skipEvent = false;
+        private readonly List<BaseSizeAdjustmentEntry> Adjustments = new();
+        public class BaseSizeAdjustmentEntry {
+            public EntityFactRef Source;
+            public int SizeDelta;
+            public Size Size;
+            public ChangeType Type;
+            public BaseSizeAdjustmentEntry(int sizeDelta, EntityFact source) {
+                Source = new EntityFactRef(source);
+                SizeDelta = sizeDelta;
+                Type = ChangeType.Delta;
+            }
+            public BaseSizeAdjustmentEntry(Size size, EntityFact source) {
+                Source = new EntityFactRef(source);
+                Size = size;
+                Type = ChangeType.Value;
+            }
+        }
+        public enum ChangeType {
+            Delta,
+            Value
+        }
+
+        [HarmonyPatch(typeof(UnitDescriptor), nameof(UnitDescriptor.OriginalSize), MethodType.Getter)]
+        static class UnitDescriptor_UnitPartBaseSizeAdjustment_Patch {
+            static void Postfix(ref Size __result, UnitDescriptor __instance) {
+                var unitPart = __instance.Get<UnitPartBaseSizeAdjustment>();
+                if (unitPart == null) { return; }
+                __result = unitPart.GetAdjustedSize(__result);
+            }
+        }
+    }
+}
