5d2baf28e1ea2141ec9f2909a52622be4361cbcb
Sean Petrie <sean.petrie17@gmail.com>
2022-04-15 23:45:16 -0500

    More config options

--- TabletopTweaks-Core/NewComponents/AbilitySpecific/TricksterLoreNatureRestLootTriggerTTT.cs
+++ TabletopTweaks-Core/NewComponents/AbilitySpecific/TricksterLoreNatureRestLootTriggerTTT.cs
@@ -3,9 +3,11 @@ using Kingmaker.Blueprints.Facts;
 using Kingmaker.Blueprints.Items.Equipment;
 using Kingmaker.Blueprints.JsonSystem;
 using Kingmaker.Controllers.Rest;
+using Kingmaker.EntitySystem.Entities;
 using Kingmaker.PubSubSystem;
 using Kingmaker.UnitLogic;
 using Kingmaker.Utility;
+using System.Collections.Generic;
 using System.Linq;
 
 
@@ -23,14 +25,27 @@ namespace TabletopTweaks.Core.NewComponents.AbilitySpecific {
             if (status.Result != RestResult.Success) {
                 return;
             }
-            var targetItem = LootList
-                .Where(item => item.CR >= (CROffset + Owner.Progression.MythicLevel) && item.CR <= (CROffset + CRRange + Owner.Progression.MythicLevel))
-                .Where(item => item.Cost >= CostFloor)
-                .Random();
-            base.Owner.Inventory.Add(targetItem);
+            List<BlueprintItemEquipment> LootResults = new List<BlueprintItemEquipment>();
+            int timesRolled = 0;
+            do {
+                LootResults.Add(LootList
+                        .Where(item => item.CR >= (CROffset + Owner.Progression.MythicLevel) && item.CR <= (CROffset + CRRange + Owner.Progression.MythicLevel))
+                        .Where(item => item.Cost >= CostFloor)
+                        .Random());
+                timesRolled++;
+            } while (timesRolled < TableRolls);
+            var SortedLootResults = LootResults.OrderBy(o => o.Cost).ToStack();
+            int addedItems = 0;
+            do {
+                if (!SortedLootResults.Any()) { return; }
+                base.Owner.Inventory.Add(SortedLootResults.Pop());
+                addedItems++;
+            } while (addedItems < AddBest);
         }
 
         public BlueprintItemEquipmentReference[] m_LootList = new BlueprintItemEquipmentReference[0];
+        public int TableRolls = 1;
+        public int AddBest = 1;
         public int CROffset = 5;
         public int CRRange = 15;
         public int CostFloor = 20000;
