ea4381def942201b9da70cee5ecd4fea0c2236cc
Sean Petrie <sean.petrie17@gmail.com>
2022-05-07 23:35:00 -0500

    New component

---
+++ TabletopTweaks-Core/NewComponents/AddSneakAttackDamageTrigger.cs
@@ -0,0 +1,29 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.ElementsSystem;
+using Kingmaker.EntitySystem;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules.Damage;
+using Kingmaker.UnitLogic.Abilities;
+using System.Linq;
+
+namespace TabletopTweaks.Core.NewComponents {
+    [AllowMultipleComponents]
+    [TypeId("ab899b2357d14767bb091548f9cee543")]
+    public class AddSneakAttackDamageTrigger : EntityFactComponentDelegate, 
+        IInitiatorRulebookHandler<RulePrepareDamage>, 
+        IRulebookHandler<RulePrepareDamage>, 
+        ISubscriber, IInitiatorRulebookSubscriber {
+
+        public void OnEventAboutToTrigger(RulePrepareDamage evt) {
+        }
+
+        public void OnEventDidTrigger(RulePrepareDamage evt) {
+            if (evt.ParentRule.DamageBundle.Any(baseDamage => baseDamage.Sneak) && !evt.ParentRule.DisablePrecisionDamage) {
+                base.Fact.RunActionInContext(Action, evt.Target);
+            }
+        }
+
+        public ActionList Action;
+    }
+}
