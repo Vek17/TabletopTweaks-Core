fdc8d5d79a71d75e9cf3af73dad519c2af97e5e7
Sean Petrie <sean.petrie17@gmail.com>
2022-05-24 23:08:25 -0500

    Added support for special capstone rules

---
+++ TabletopTweaks-Core/NewComponents/AbilitySpecific/AddFeatureOnClassLevelToPlayers.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.UnitLogic;
+
+namespace TabletopTweaks.Core.NewComponents.AbilitySpecific {
+
+    [TypeId("fee1dc0c040845ba8abcdca526d0f52e")]
+    public class AddFeatureOnClassLevelToPlayers : AddFeatureOnClassLevel {
+
+        private new bool IsFeatureShouldBeApplied() {
+            return IsValidUnit(base.Owner);
+        }
+
+        private bool IsValidUnit(UnitDescriptor unit) {
+            if (unit == null) { return false; }
+            var result = unit.Unit.IsMainCharacter
+                || unit.Unit.IsCloneOfMainCharacter
+                || unit.Unit.IsStoryCompanion()
+                || unit.Unit.IsCustomCompanion()
+                || (unit.Blueprint?.IsCompanion ?? false);
+            return Not ? !result : result;
+        }
+        public bool Not = false;
+
+        [HarmonyPatch(typeof(AddFeatureOnClassLevel), nameof(AddFeatureOnClassLevel.IsFeatureShouldBeApplied))]
+        static class LocalizationManager_OnLocaleChanged_Handler {
+            static void Postfix(AddFeatureOnClassLevel __instance, ref bool __result) {
+                var componet = __instance as AddFeatureOnClassLevelToPlayers;
+                if (componet != null) { __result &= componet.IsFeatureShouldBeApplied(); }
+            }
+        }
+    }
+}
---
+++ TabletopTweaks-Core/NewComponents/Prerequisites/PrerequisiteOracleMystery.cs
@@ -0,0 +1,56 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Blueprints.Root.Strings;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Class.LevelUp;
+using System.Text;
+
+namespace TabletopTweaks.Core.NewComponents.Prerequisites {
+    [TypeId("064cb6d3927e4972a6d2846fb4fef335")]
+    public class PrerequisiteOracleMystery : Prerequisite {
+
+        public ReferenceArrayProxy<BlueprintFeature, BlueprintFeatureReference> Features => m_Features;
+        public BlueprintFeature Bypass => m_BypassFeature?.Get();
+
+        public override bool ConsiderFulfilled(FeatureSelectionState selectionState, UnitDescriptor unit, LevelUpState state) {
+            //Handles alternate capstone
+            return Bypass != null && unit.Progression.CharacterLevel == 20 && unit.Progression.Features.HasFact(Bypass);
+        }
+
+        public override bool CheckInternal(FeatureSelectionState selectionState, UnitDescriptor unit, LevelUpState state) {
+            int num = 0;
+            foreach (BlueprintFeature blueprintFeature in this.Features) {
+                if ((!(selectionState != null) || !selectionState.IsSelectedInChildren(blueprintFeature)) && unit.HasFact(blueprintFeature)) {
+                    num++;
+                    if (num >= this.Amount) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public override string GetUITextInternal(UnitDescriptor unit) {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (this.Amount <= 1) {
+                stringBuilder.Append(string.Format("{0}:\n", UIStrings.Instance.Tooltips.OneFrom));
+            } else {
+                stringBuilder.Append(string.Format(UIStrings.Instance.Tooltips.FeaturesFrom, this.Amount));
+                stringBuilder.Append(":\n");
+            }
+            for (int i = 0; i < this.Features.Length; i++) {
+                stringBuilder.Append(this.Features[i].Name);
+                if (i < this.Features.Length - 1) {
+                    stringBuilder.Append("\n");
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+        public BlueprintFeatureReference[] m_Features;
+        public BlueprintFeatureReference m_BypassFeature;
+        public int Amount = 1;
+    }
+}
