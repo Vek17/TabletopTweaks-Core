603da2f937c3941ea06be4e7e8e5f3ade11f09d1
Sean Petrie <sean.petrie17@gmail.com>
2022-05-25 21:24:36 -0500

    Modified to support progress and selection exclusion

--- TabletopTweaks-Core/NewComponents/Prerequisites/PrerequisiteInPlayerParty.cs
+++ TabletopTweaks-Core/NewComponents/Prerequisites/PrerequisiteInPlayerParty.cs
@@ -1,4 +1,7 @@
-﻿using Kingmaker.Blueprints.Classes.Prerequisites;
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using Kingmaker.Blueprints.Classes.Selection;
 using Kingmaker.Blueprints.JsonSystem;
 using Kingmaker.UnitLogic;
 using Kingmaker.UnitLogic.Class.LevelUp;
@@ -6,13 +9,17 @@ using Kingmaker.UnitLogic.Class.LevelUp;
 namespace TabletopTweaks.Core.NewComponents.Prerequisites {
     [TypeId("cb189aeff170432c856d0f3f837ddf4f")]
     public class PrerequisiteInPlayerParty : Prerequisite {
+        private BlueprintFeature BypassInSelection => m_BypassInSelection?.Get();
+
         public override bool CheckInternal(FeatureSelectionState selectionState, UnitDescriptor unit, LevelUpState state) {
             var result = unit.Unit.IsMainCharacter
                 || unit.Unit.IsCloneOfMainCharacter
                 || unit.Unit.IsStoryCompanion()
                 || unit.Unit.IsCustomCompanion()
                 || (unit.Blueprint?.IsCompanion ?? false);
-            return Not ? !result : result;
+            var ignore = (unit.Progression.CharacterLevel < IgnoreLevelsBelow)
+                || BypassInSelection is not null ? (selectionState?.Selection as BlueprintFeatureSelection)?.AssetGuid == BypassInSelection.AssetGuid : false;
+            return ignore || (Not ? !result : result);
         }
 
         public override string GetUITextInternal(UnitDescriptor unit) {
@@ -20,5 +27,7 @@ namespace TabletopTweaks.Core.NewComponents.Prerequisites {
         }
 
         public bool Not = false;
+        public int IgnoreLevelsBelow = 0;
+        public BlueprintFeatureSelectionReference m_BypassInSelection;
     }
 }
