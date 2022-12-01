88b51767506e0b27d01c22389033a01bbb27ec99
alterasc <alterasc@protonmail.com>
2022-12-01 05:16:08 +0100

    add support for descriptor immunity ignore

---
+++ TabletopTweaks-Core/NewComponents/BuffDescriptorImmunityIgnore.cs
@@ -0,0 +1,20 @@
+﻿using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.UnitLogic;
+using TabletopTweaks.Core.NewUnitParts;
+
+namespace TabletopTweaks.Core.NewComponents {
+    [TypeId("ebd1d39947344e15a478755d551b2a46")]
+    public class BuffDescriptorImmunityIgnore : UnitFactComponentDelegate {
+
+        public SpellDescriptorWrapper Descriptor;
+        public override void OnTurnOn() {
+            base.Owner.Ensure<UnitPartIgnoreBuffDescriptorImmunity>().AddEntry(Descriptor, base.Fact);
+        }
+
+        public override void OnTurnOff() {
+            base.Owner.Ensure<UnitPartIgnoreBuffDescriptorImmunity>().RemoveEntry(base.Fact);
+        }
+
+    }
+}
---
+++ TabletopTweaks-Core/NewComponents/SpellDescriptorImmunityIgnore.cs
@@ -0,0 +1,25 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Blueprints.Facts;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Parts;
+
+namespace TabletopTweaks.Core.NewComponents {
+
+    [AllowMultipleComponents]
+    [AllowedOn(typeof(BlueprintUnitFact), false)]
+    [TypeId("fa39b200501a498c95c16d36612f5bf7")]
+    public class SpellDescriptorImmunityIgnore :
+    UnitFactComponentDelegate {
+        public SpellDescriptorWrapper Descriptor;
+
+        public override void OnTurnOn() {
+            Owner.Ensure<UnitPartSpellResistance>().IgnoreImmunity(Descriptor);
+        }
+
+        public override void OnTurnOff() {
+            Owner.Ensure<UnitPartSpellResistance>().RestoreImmunity(Descriptor);
+        }
+    }
+}
