9d8509cad78d30e300648b7f3ed2e3e76875b6f3
Sean Petrie <sean.petrie17@gmail.com>
2023-03-11 11:16:22 -0600

    Support for winter Witch

---
+++ TabletopTweaks-Core/NewComponents/ChangeSpellHalfDamageIgnoreImmunityAndResist.cs
@@ -0,0 +1,63 @@
+﻿using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Blueprints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingmaker.EntitySystem;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.RuleSystem.Rules.Damage;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.Enums.Damage;
+using Kingmaker.UnitLogic.Mechanics;
+using System.Runtime.Remoting.Contexts;
+using Kingmaker.UnitLogic;
+using Kingmaker.Utility;
+using Kingmaker.Blueprints.Facts;
+using Kingmaker.Enums;
+using Kingmaker.Settings;
+using static Kingmaker.Blueprints.BlueprintUnitTemplate;
+using UnityEngine;
+using static LayoutRedirectElement;
+
+namespace TabletopTweaks.Core.NewComponents {
+    [AllowMultipleComponents]
+    [TypeId("b6ae817008344774b06eeef4364fe6c4")]
+    public class ChangeSpellHalfDamageIgnoreImmunityAndResist : EntityFactComponentDelegate,
+        IInitiatorRulebookHandler<RulePrepareDamage>,
+        IRulebookHandler<RulePrepareDamage>,
+        ISubscriber, IInitiatorRulebookSubscriber {
+
+        public void OnEventAboutToTrigger(RulePrepareDamage evt) {
+        }
+
+        public void OnEventDidTrigger(RulePrepareDamage evt) {
+            var Type = evt.ParentRule?.Reason?.Ability?.Blueprint.Type ?? (AbilityType)(-1);
+            if (CheckAbilityType && !ValidAbilityTypes.Contains(Type)) {
+                return;
+            }
+            foreach (BaseDamage baseDamage in evt.DamageBundle) {
+                if (baseDamage.Type == DamageType.Energy) {
+                    EnergyDamage energyDamage = baseDamage as EnergyDamage;
+                    if (energyDamage == null) { continue; }
+                    if (energyDamage.EnergyType == EnergyType && !baseDamage.Precision) {
+                        EnergyDamage newDamage = new EnergyDamage(baseDamage.Dice.ModifiedValue, EnergyType);
+                        baseDamage.Half.Set(true, this.Fact);
+
+                        newDamage.CopyFrom(baseDamage);
+                        newDamage.SourceFact = this.Fact;
+                        newDamage.IgnoreImmunities = true;
+                        newDamage.IgnoreReduction = true;
+                        evt.Add(newDamage);
+                    }
+                }
+            }
+        }
+        public DamageEnergyType EnergyType;
+        public bool CheckAbilityType = false;
+        public AbilityType[] ValidAbilityTypes = new AbilityType[0];
+    }
+}
---
+++ TabletopTweaks-Core/NewComponents/PartialEnergyResistanceIgnore.cs
@@ -0,0 +1,62 @@
+﻿using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Blueprints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingmaker.EntitySystem;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.RuleSystem.Rules.Damage;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.Enums.Damage;
+using Kingmaker.UnitLogic.Mechanics;
+using System.Runtime.Remoting.Contexts;
+using Kingmaker.UnitLogic;
+using Kingmaker.Utility;
+using Kingmaker.Blueprints.Facts;
+using Kingmaker.Enums;
+using Kingmaker.Settings;
+using static Kingmaker.Blueprints.BlueprintUnitTemplate;
+using UnityEngine;
+using static LayoutRedirectElement;
+
+namespace TabletopTweaks.Core.NewComponents {
+    [AllowMultipleComponents]
+    [TypeId("b25a641d55694b5eb9bf5293be79292f")]
+    public class PartialEnergyResistanceIgnore : EntityFactComponentDelegate,
+        IInitiatorRulebookHandler<RuleCalculateDamage>,
+        IRulebookHandler<RuleCalculateDamage>,
+        ISubscriber, IInitiatorRulebookSubscriber {
+
+        public void OnEventAboutToTrigger(RuleCalculateDamage evt) {
+        }
+
+        public void OnEventDidTrigger(RuleCalculateDamage evt) {
+            var Type = evt.ParentRule?.Reason?.Ability?.Blueprint.Type ?? (AbilityType)(-1);
+            if (CheckAbilityType && !ValidAbilityTypes.Contains(Type)) {
+                return; 
+            }
+            foreach (DamageValue damageValue in evt.CalculatedDamage) {
+                EnergyDamage source = damageValue.Source as EnergyDamage;
+                if (source != null) {
+                    int Reduction = source.ReductionBecauseResistance;
+                    if (Reduction > 0  && source.EnergyType == EnergyType) {
+                        if (ByHalf) {
+                            source.ReductionPenalty.Add(new Modifier(Mathf.CeilToInt((float)Reduction / 2f), this.Fact, ModifierDescriptor.UntypedStackable));
+                        } else {
+                            source.ReductionPenalty.Add(new Modifier(Value.Calculate(this.Context), this.Fact, ModifierDescriptor.UntypedStackable));
+                        }
+                    }
+                }
+            }
+        }
+        public DamageEnergyType EnergyType;
+        public bool ByHalf = false;
+        public ContextValue Value = new ContextValue();
+        public bool CheckAbilityType = false;
+        public AbilityType[] ValidAbilityTypes = new AbilityType[0];
+    }
+}
