e65243499c44229f194d27eef9143e6924f433e6
Sean Petrie <sean.petrie17@gmail.com>
2023-03-13 22:59:25 -0500

    Cleaned up

--- TabletopTweaks-Core/NewComponents/BurningMagic.cs
+++ TabletopTweaks-Core/NewComponents/BurningMagic.cs
@@ -50,6 +50,7 @@ namespace TabletopTweaks.Core.NewComponents {
             );
             var spellLevel = context.Params?.SpellLevel ?? context?.SpellLevel;
             fakeContext.RecalculateAbilityParams();
+            fakeContext.ParentContext = context;
             fakeContext.Params.CasterLevel = spellLevel ?? 1;
             fakeContext.Params.Metamagic = 0;
             var appliedBuff = evt.Target?.Descriptor?.AddBuff(Buff, fakeContext, Duration.Calculate(fakeContext).Seconds);
