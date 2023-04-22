1904d8e9db1f3c27c445fc2ddd60a3136a1ca3c9
Sean Petrie <sean.petrie17@gmail.com>
2023-04-22 01:40:17 -0500

    Support for improved natural attack

---
+++ TabletopTweaks-Core/NewComponents/WeaponChangeSizeParametrized.cs
@@ -0,0 +1,35 @@
+﻿using Kingmaker.Blueprints.Items.Weapons;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.UnitLogic;
+using Kingmaker.Blueprints.JsonSystem;
+
+namespace TabletopTweaks.Core.NewComponents {
+    [TypeId("66a9a23164554207bf8245d3417f3dec")]
+    public class WeaponChangeSizeParametrized : UnitFactComponentDelegate,
+        IInitiatorRulebookHandler<RuleCalculateWeaponSizeBonus>,
+        IRulebookHandler<RuleCalculateWeaponSizeBonus> {
+
+        public void OnEventAboutToTrigger(RuleCalculateWeaponSizeBonus evt) {
+        }
+
+        public void OnEventDidTrigger(RuleCalculateWeaponSizeBonus evt) {
+            if (IsValidWeapon(evt.Weapon.Blueprint)) {
+                var preSize = evt.WeaponSize;
+                evt.m_SizeShift += SizeChange.Calculate(base.Context);
+                evt.WeaponDamageDice.Modify(
+                   WeaponDamageScaleTable.Scale(evt.WeaponDamageDice.ModifiedValue, evt.WeaponSize, preSize, evt.Weapon.Blueprint), base.Fact
+                );
+            }
+        }
+
+        private bool IsValidWeapon(BlueprintItemWeapon weapon) {
+            if (weapon.Category != base.Param) { return false; }
+
+            return true;
+        }
+
+        public ContextValue SizeChange = 1;
+    }
+}
