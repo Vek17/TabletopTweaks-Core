14583fb9607fdfe94cc9b0869a600c2bb7c04e89
Sean Petrie <sean.petrie17@gmail.com>
2023-10-01 15:41:30 -0500

    New support for context caster level bonuses

---
+++ TabletopTweaks-Core/NewComponents/AddContextCasterLevelBonus.cs
@@ -0,0 +1,40 @@
+﻿using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.Enums;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules.Abilities;
+using Kingmaker.UnitLogic.Parts;
+using Kingmaker.UnitLogic;
+using Kingmaker.Utility;
+
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.Blueprints;
+
+namespace TabletopTweaks.Core.NewComponents {
+    [TypeId("4494a51dbdfe493b9e9a95bafb4b1a4c")]
+    public class AddContextCasterLevelBonus : UnitFactComponentDelegate, IInitiatorRulebookHandler<RuleCalculateAbilityParams>, IRulebookHandler<RuleCalculateAbilityParams>, ISubscriber, IInitiatorRulebookSubscriber {
+        public void OnEventAboutToTrigger(RuleCalculateAbilityParams evt) {
+            if (this.SpellsOnly && evt.Spellbook == null) {
+                return;
+            }
+            SpellDescriptorComponent component = evt.Spell.GetComponent<SpellDescriptorComponent>();
+            if (component == null) {
+                return;
+            }
+            SpellDescriptor spellDescriptor = component.Descriptor.Value;
+            spellDescriptor = UnitPartChangeSpellElementalDamage.ReplaceSpellDescriptorIfCan<UnitEntityData>(base.Owner, spellDescriptor);
+            if (spellDescriptor.HasAnyFlag(this.Descriptor)) {
+                evt.AddBonusCasterLevel(this.BonusCasterLevel.Calculate(this.Context), this.ModifierDescriptor);
+            }
+        }
+
+        public void OnEventDidTrigger(RuleCalculateAbilityParams evt) {
+        }
+
+        public SpellDescriptorWrapper Descriptor;
+        public ContextValue BonusCasterLevel;
+        public ModifierDescriptor ModifierDescriptor = ModifierDescriptor.UntypedStackable;
+        public bool SpellsOnly;
+    }
+}
