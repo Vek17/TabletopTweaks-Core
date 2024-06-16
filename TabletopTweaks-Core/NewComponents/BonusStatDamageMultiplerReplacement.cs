d2b2a7878ce5204dfe3c84bf783390fe30a6b4b2
Sean Petrie <sean.petrie17@gmail.com>
2024-06-16 17:02:10 -0500

    Added new compons for weapon weapon overrieds

---
+++ TabletopTweaks-Core/NewComponents/BonusStatDamageMultiplerReplacement.cs
@@ -0,0 +1,27 @@
+﻿using Kingmaker.Blueprints.Items.Ecnchantments;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Enums;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic;
+
+namespace TabletopTweaks.Core.NewComponents {
+    [TypeId("eceed4ce1340485195bb13ce1f0ac06c")]
+    public class BonusStatDamageMultiplerReplacement : UnitFactComponentDelegate,
+        IInitiatorRulebookHandler<RuleCalculateWeaponStats>,
+        IRulebookHandler<RuleCalculateWeaponStats>,
+        ISubscriber, IInitiatorRulebookSubscriber {
+
+        public void OnEventAboutToTrigger(RuleCalculateWeaponStats evt) {
+            if (CheckWeaponCategory && evt.Weapon.Blueprint.Category != WeaponCategory) { return; }
+            evt.OverrideDamageBonusStatMultiplier(this.Multiplier);
+        }
+
+        public void OnEventDidTrigger(RuleCalculateWeaponStats evt) {
+        }
+
+        public float Multiplier;
+        public bool CheckWeaponCategory;
+        public WeaponCategory WeaponCategory;
+    }
+}
---
+++ TabletopTweaks-Core/NewComponents/SecondaryWeaponOverride.cs
@@ -0,0 +1,26 @@
+﻿using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Enums;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic;
+
+namespace TabletopTweaks.Core.NewComponents {
+    [TypeId("5b61a8c461f941f497abf23f24abd66a")]
+    public class SecondaryWeaponOverride : UnitFactComponentDelegate,
+        IInitiatorRulebookHandler<RuleCalculateWeaponStats>,
+        IRulebookHandler<RuleCalculateWeaponStats>,
+        ISubscriber, IInitiatorRulebookSubscriber {
+
+        public void OnEventAboutToTrigger(RuleCalculateWeaponStats evt) {
+            if (CheckWeaponCategory && evt.Weapon.Blueprint.Category != WeaponCategory) { return; }
+            evt.IsSecondaryOverride = IsSecondary;
+        }
+
+        public void OnEventDidTrigger(RuleCalculateWeaponStats evt) {
+        }
+
+        public bool IsSecondary;
+        public bool CheckWeaponCategory;
+        public WeaponCategory WeaponCategory;
+    }
+}
