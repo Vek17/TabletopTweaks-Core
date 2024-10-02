46ec24636022c35bb78db347a2e3db1b1bbea038
Sean Petrie <sean.petrie17@gmail.com>
2024-10-02 09:17:32 -0500

    Updated class name

--- TabletopTweaks-Core/NewEvents/IAbilityGetCommandTypeHandler.cs
+++ TabletopTweaks-Core/NewEvents/IAbilityGetCommandTypeHandler.cs
@@ -8,7 +8,7 @@ namespace TabletopTweaks.Core.NewEvents {
         void HandleGetCommandType(AbilityData ability, ref UnitCommand.CommandType commandType);
 
         [HarmonyPatch(typeof(AbilityData), nameof(AbilityData.ActionType), MethodType.Getter)]
-        static class UnitActivateAbility_GetCommandType_IActivatableAbilityGetCommandTypeHandler_Patch {
+        static class AbilityData_ActionType_IAbilityGetCommandTypeHandler_Patch {
             static void Postfix(AbilityData __instance, ref UnitCommand.CommandType __result) {
                 var result = __result;
                 EventBus.RaiseEvent<IAbilityGetCommandTypeHandler>(__instance.Caster, h => h.HandleGetCommandType(__instance, ref result));
